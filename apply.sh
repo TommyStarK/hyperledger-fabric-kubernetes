6f55912b7ff1917b427fd20f04aa518388d9a909
TommyStarK <thomasmilox@gmail.com>
2021-01-20 18:42:04 +0100

    update script according new architecture

--- apply.sh
+++ apply.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 kubectl apply -f namespaces/
-kubectl apply -f secrets/org1
+kubectl apply -f secrets/org1.dummy.com/
 kubectl apply -f services/
-kubectl apply -f services/org1
-kubectl apply -f services/org2
-kubectl apply -f services/org3
+kubectl apply -f services/org1.dummy.com/
+kubectl apply -f services/org2.dummy.com/
+kubectl apply -f services/org3.dummy.com/
 kubectl apply -f deployments/
-kubectl apply -f deployments/org1
-kubectl apply -f deployments/org2
-kubectl apply -f deployments/org3
-
+kubectl apply -f deployments/org1.dummy.com/
+kubectl apply -f deployments/org2.dummy.com/
+kubectl apply -f deployments/org3.dummy.com/
