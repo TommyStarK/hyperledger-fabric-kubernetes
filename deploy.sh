01d3328d291becd1fd16c402e27bc276b1a25b9b
TommyStarK <thomasmilox@gmail.com>
2021-04-24 20:31:42 +0200

    k8s: setup job, update git init container. Update deploy script to check that all pods are up and running.
    Signed-off-by: TommyStarK <thomasmilox@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ kubectl apply -f k8s/storage/pv;
 kubectl apply -f k8s/storage/pvc;
 kubectl apply -f k8s/job;
 kubectl wait --timeout=120s --for=condition=complete --namespace dummy-com job.batch/setup;
+kubectl delete -f k8s/job;
 kubectl apply -k k8s;
+kubectl wait --timeout=180s --for=condition=Ready --namespace dummy-com pods --all;
